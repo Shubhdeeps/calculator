fbd8cad3482f41aa946c002733f5ed86f3c3f930
Shubhdeep Singh <shubhdeep86993@gmail.com>
2022-11-06 14:36:27 +0200

    resolve conflicts

---
+++ lib/screens/History.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../components/components.dart';
+
+class History extends StatelessWidget {
+  const History({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color(0xFF1A2940),
+                Color(0xFF2A4767),
+              ],
+              begin: FractionalOffset(0.5, 1.0),
+              end: FractionalOffset(0.5, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [],
+        ),
+      ),
+    );
+    ;
+  }
+}
